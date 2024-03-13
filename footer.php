d1c70adf8fa22543c5ffb3a1a79ee56bad6f141b
i-okushima <i.okushima@gmail.com>
2024-03-14 00:21:35 +0900

    Initial commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="content_inner">テーマ切り替え検証サイト<br>－ 自動版 －</div>
+</footer>
+</body>
+
+</html>
---
+++ page-div_elements.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Template Name: div要素
+ */
+?>
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="title_area">
+        <div class="content_inner"><?php echo get_the_title(); ?></div>
+    </div>
+    <main>
+        <section>
+            <div class="SurfaceContainer">
+                <div class="SurfaceContainer">
+                    <div class="SurfaceContainer">
+                        <div class="SurfaceContainer">
+                            <div class="SurfaceContainer">
+                                <span>div要素の重なり</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="text_wrapper_inner">
+                <img src="https://www.hitachi-solutions-create.co.jp/column/img/image-generation-ai.jpg" width="100%" />
+            </div>
+        </section>
+    </main>
+</div>
+<?php get_footer(); ?>
